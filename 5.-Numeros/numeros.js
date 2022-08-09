51af754d90761477c12fd21de061097ea6ffb06f
LeoEstaProgramando <107797348+LeoEstaProgramando@users.noreply.github.com>
2022-08-09 02:04:50 -0500

    Update numeros.js

--- 5.-Numeros/numeros.js
+++ 5.-Numeros/numeros.js
@@ -8,12 +8,12 @@ let altura_metros = 1.67;
 let peso_kg = 59.95;
 
 //Una variable que contenga tu altura en metros redondeada hacia arriba
-let altura_metros_top = Number(altura_metros.toFixed(0));
+let altura_metros_top = Math.ceil(altura_metros);
 console.log(altura_metros_top);
 
 //Una variable que contenga tu peso en kilogramos redondeado hacia abajo
-let altura_metros_bot = Number(parseInt(altura_metros));
-console.log(altura_metros_bot);
+let peso_kg_floor = Math.floor(peso_kg);
+console.log(peso_kg_floor);
 
 //Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
 let verify_max_value_JS = Number.MAX_VALUE == Number.MAX_VALUE + 1;
